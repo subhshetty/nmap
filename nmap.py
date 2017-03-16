8d2e02cb2d4d75fded741532b0d00ce0bd4abb77
subhshetty <subhash.shetty@mavs.uta.edu>
2017-03-16 13:08:56 -0500

    Add files via upload

---
+++ nmap.py
@@ -0,0 +1,30 @@
+import nmap
+
+def get_mac(ip):
+	nm = nmap.PortScanner()
+	host = (ip)
+
+	a=nm.scan(hosts=host, arguments='-sP')
+
+	for k,v in a['scan'].iteritems():
+    		if str(v['status']['state']) == 'up':
+        		print str(v)
+        		try:    print str(v['addresses']['ipv4']) + ' => ' + str(v['addresses']['fqdn'])
+        		except: print str(v['addresses']['ipv4'])
+get_mac(ip=<target>)
+	
+
+import nmap
+
+	def get_fqdn(ip):
+    	nm = nmap.PortScanner()
+    	host = (ip)
+
+    	a=nm.scan(hosts=host, arguments='-sP')
+
+    	for k,v in a['scan'].iteritems():
+        	if str(v['status']['state']) == 'up':
+            		print str(v)
+            		try:    print str(v['addresses']['ipv4']) + ' => ' + str(v['addresses']['fqdn'])
+            		except: print str(v['addresses']['ipv4'])
+get_fqdn(ip='8.8.8.8')
